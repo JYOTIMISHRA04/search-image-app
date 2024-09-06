31db14fe10954e984b203952bfa90460bdf2103a
Jyoti mishra <jyotimishra0896@gmail.com>
2024-09-06 23:05:42 +0530

    app title updated

--- src/components/app-bar/AppBar.js
+++ src/components/app-bar/AppBar.js
@@ -15,7 +15,7 @@ const AppBarComponent = () => {
             <MenuIcon />
           </IconButton>
           <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-            Search Image Assignment
+            Search Image App
           </Typography>
         </Toolbar>
       </AppBar>
